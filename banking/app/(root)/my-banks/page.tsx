e06f5b8d68ad75c76e4fbeeb658df53206a25fde
Shirsendu Das <shirsendudas49@gmail.com>
2025-03-18 01:09:19 +0530

    Imlemnet routs and create the home page

---
+++ banking/app/(root)/my-banks/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const myBanks = () => {
+  return (
+    <div>
+      MyBanks
+    </div>
+  )
+}
+
+export default myBanks
--- banking/app/(root)/page.tsx
+++ banking/app/(root)/page.tsx
@@ -1,10 +1,38 @@
 import React from 'react'
-
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
+import RightSidebar from '@/components/RightSidebar'
 const page = () => {
+  const LogIn = {firstname : 'Jenny'}
   return (
-    <div>
-      Home
-    </div>
+    <>
+      <section  className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user={LogIn?.firstname || 'Guest'}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+
+        <TotalBalanceBox 
+        accounts={[]}
+        totalBanks={1}
+        totalCurrentBalance={1250.35}
+        />
+        </header>
+        RECENT TRANSACTION
+        
+      </div>
+      <RightSidebar 
+      user={LogIn}
+      transactions={[]}
+      banks={[{currentBalance:123.50},{currentBalance:200.50}]}
+      />
+      
+    </section>
+    </>
   )
 }
 
